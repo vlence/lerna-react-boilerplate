2f98a32324db031566b268aa656a96d40ceda636
Victor Basumatary <vlenceleuth@gmail.com>
2019-03-14 13:47:33 +0530

    Package name is validated while creating component

--- scripts/src/create-component.js
+++ scripts/src/create-component.js
@@ -4,6 +4,7 @@ const path = require('path');
 const lernaCreate = require('./common/lerna-create');
 const runCommand = require('./common/run-command');
 const root = require('./common/root');
+const isValid = require('./common/is-valid-package-name');
 
 // Identify all components by scope @example-app-components
 const namespace = require('./common/create-scope-namespace')('components');
@@ -68,6 +69,11 @@ const install = name => {
  * @returns {Promise}
  */
 const createComponent = async name => {
+  const packageName = `${namespace}/${name}`;
+  if (!isValid(packageName)) {
+    throw new SyntaxError(`"${packageName}" is not a valid name`);
+  }
+
   await newReactComponent(name);
   await lernaCreate(namespace, name, 'components');
 
--- scripts/src/create-component.test.js
+++ scripts/src/create-component.test.js
@@ -1,22 +1,30 @@
-const createComponent = require('./create-component');
-const rimraf = require('rimraf');
-const root = require('./common/root');
 const path = require('path');
+const promisify = require('util').promisify;
+const rimraf = promisify(require('rimraf'));
+
+const root = require('./common/root');
+const addScopeNamespace = require('./add-scope-namespace');
+const removeProjectScope = require('./remove-project-scope');
 
 const componentName = 'test';
 
+beforeAll(() => addScopeNamespace('@test'));
+afterAll(() => removeProjectScope());
+
 jest.setTimeout(1000*60*2);
 describe('create-component', () => {
-  it('should create component', () => createComponent(componentName));
   
-  afterAll(() => new Promise((resolve, reject) => {
-    rimraf(path.resolve(root, 'components', componentName), err => {
-      if (err) {
-        reject(err);
-      }
-      else {
-        resolve();
-      }
-    });
-  }));
+  // We do this because create-component.js requires
+  // a non-existent file. The file is only created
+  // after we run beforeAll().
+  let createComponent;
+  beforeAll(() => createComponent = require('./create-component'));
+
+  afterAll(() => rimraf(path.resolve(root, 'components', componentName)));
+
+  it('should fail if package name is invalid', () => {
+    expect(createComponent('invalid!')).rejects.toThrow();
+  });
+
+  it('should create component', () => createComponent(componentName));
 });
